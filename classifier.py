c72b13a4370db7a1a4c3870a3d814dfc3c8e38f2
Maaz Ali <maazali22@gmail.com>
2023-05-20 19:15:54 -0400

    added create dataset

--- classifier.py
+++ classifier.py
@@ -9,6 +9,24 @@ X = [
     [-1.5, 2.7, 3.3, -0.8]
 ] # shape: 3,4,1
 
+def create_data(points, classes):
+    # np.random.seed(0) # set seed for reproducibility
+
+    # create data and labels
+    X = np.zeros((points * classes, 2)) # data matrix (each row = single example)
+    y = np.zeros(points * classes, dtype='uint8') # class labels
+
+    for class_number in range(classes):
+        ix = range(points * class_number, points * (class_number + 1))
+        r = np.linspace(0.0, 1, points) # radius
+        t = np.linspace(class_number * 4, (class_number + 1) * 4, points) + np.random.randn(points) * 0.2 # theta
+        X[ix] = np.c_[r * np.sin(t * 2.5), r * np.cos(t * 2.5)]
+        y[ix] = class_number
+
+    return X, y
+
+X, y = create_data(100, 3)
+
 class Layer_Dense:
     # initialize the weights and biases
     # best practice to initialize weights with random values between -1 and 1
