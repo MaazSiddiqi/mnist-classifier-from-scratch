785f204455a0a537e64020871fc5882be8bb4fd8
Maaz Ali <maazali22@gmail.com>
2023-05-20 16:54:32 -0400

    added another layer

--- classifier.py
+++ classifier.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5] # shape: 4,1
+# inputs = [1, 2, 3, 2.5] # shape: 4,1
+inputs = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+] # shape: 3,4,1
 
 weights = [
     [0.2, 0.8, -0.5, 1],
@@ -10,6 +15,15 @@ weights = [
 
 biases = [ 2, 3, 0.5 ] # shape: 3,1
 
-output = np.dot(weights, inputs) + biases # shape: (3,4 * 4,1) + 3,1 = 3,1 + 3,1 = 3,1
+weights2 = [
+    [0.1, -0.14, 0.5],
+    [-0.5, 0.12, -0.33],
+    [-0.44, 0.73, -0.13]
+] # shape: 3,3
 
-print(output)
+biases2 = [-1, 2, -0.5] # shape: 3,1
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
