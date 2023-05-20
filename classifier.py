0eb67c31faa9613100c11e24ff5ae3d297272ad9
Maaz Ali <maazali22@gmail.com>
2023-05-20 16:13:40 -0400

    cleaning up code

--- classifier.py
+++ classifier.py
@@ -1,28 +1,19 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 
-weights1 = [0.2, 0.8, -0.5, 1]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
-
-bias1 = 2
-bias2 = 2
-bias3 = 2
-
-output = [
-    inputs[0] * weights1[0] +
-    inputs[1] * weights1[1] +
-    inputs[2] * weights1[2] +
-    inputs[3] * weights1[3] + bias1,
-
-    inputs[0] * weights2[0] +
-    inputs[1] * weights2[1] +
-    inputs[2] * weights2[2] +
-    inputs[3] * weights2[3] + bias2,
-
-    inputs[0] * weights3[0] +
-    inputs[1] * weights3[1] +
-    inputs[2] * weights3[2] +
-    inputs[3] * weights3[3] + bias3
+weights = [
+    [0.2, 0.8, -0.5, 1],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
 ]
 
-print(output)
+biases = [ 2, 3, 0.5 ]
+
+# Output of current layer
+layer_outputs = []
+for nueron_weights, bias in zip(weights, biases):
+  output = np.dot(nueron_weights, inputs) + bias
+  layer_outputs.append(output)
+
+print(layer_outputs)
