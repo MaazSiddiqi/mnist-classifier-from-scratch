74afb64be4b44be9b5f612e9d29ac6d0d4d4e270
Maaz Ali <maazali22@gmail.com>
2023-05-23 19:17:42 -0400

    removing old code

--- xor_test.py
+++ xor_test.py
@@ -18,26 +18,3 @@ network = Network(layers, mse)
 
 network.train(X, Y, epochs=10000, learning_rate=0.1, log=True)
 network.test(X, Y)
-
-# epochs = 10000
-# learning_rate = 0.1
-
-# for epoch in range(epochs):
-#     error = 0
-
-#     for x, y in zip(X, Y):
-#         # Forward Propagation
-#         outputs = x
-#         outputs = predict(outputs, network)
-
-#         # Calculate error
-#         error += mse.calculate(outputs, y)
-
-#         # Backward Propagation
-#         gradient = mse.prime(outputs, y)
-#         for layer in reversed(network):
-#             gradient = layer.backward(gradient, learning_rate)
-
-#     error /= len(X)
-
-#     print(f"{epoch + 1}/{epochs}, error={error}")
